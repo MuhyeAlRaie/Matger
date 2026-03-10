6ea7b2d8ce77585c987c91b3a1971d0310b5f0e8
Muhye AlDeen Akram Al-Raie <86913045+MuhyeAlRaie@users.noreply.github.com>
2026-03-10 06:41:20 +0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -7,6 +7,6 @@ import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2';
 
 // REPLACE THESE WITH YOUR KEYS
 const SUPABASE_URL = 'https://mpkkuhcarwfribnxaqpq.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_2fC7OROwMaR8V71RlF4RKw_llNdpbAl';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wa2t1aGNhcndmcmlibnhhcXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODUwMjQsImV4cCI6MjA4ODY2MTAyNH0.HRwGVkaTQ-EM9AEVk8MzOadrt7j1aS8tEwHrwjPM-Iw';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
