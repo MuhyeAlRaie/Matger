26ad3e8bab15d374520cfe8064313138c3eca663
Muhye AlDeen Akram Al-Raie <86913045+MuhyeAlRaie@users.noreply.github.com>
2026-03-10 06:30:02 +0300

    edit and add image

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -6,7 +6,7 @@
 import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2';
 
 // REPLACE THESE WITH YOUR KEYS
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_URL = 'https://mpkkuhcarwfribnxaqpq.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_2fC7OROwMaR8V71RlF4RKw_llNdpbAl';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
